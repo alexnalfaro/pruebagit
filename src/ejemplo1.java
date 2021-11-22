1b9c2395bfc0b454ed7c7c5684bacaca75c6c2a1
Alex <anunezal21dw|ikzubirimanteo.com>
2021-11-22 13:07:49 +0100

    Segundo commit donde añado "adios"

--- src/ejemplo1.java
+++ src/ejemplo1.java
@@ -4,6 +4,8 @@ public class ejemplo1 {
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
 		System.out.println("Hola");
+		
+		System.out.println("Adios");
 
 	}
 
